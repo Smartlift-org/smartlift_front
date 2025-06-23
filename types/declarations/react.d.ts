be2767e9cc9a082fa3b57a31b28f6ed09c71ac17
Fede <fcmde1995@gmail.com>
2025-06-23 00:05:06 -0300

    Fix TypeScript declarations: Add useRef hook definition to React module

--- types/declarations/react.d.ts
+++ types/declarations/react.d.ts
@@ -7,4 +7,6 @@ declare module 'react' {
   export function useState<T>(initialState: T | (() => T)): [T, (newState: T) => void];
   export function useLayoutEffect(effect: () => void | (() => void), deps?: ReadonlyArray<any>): void;
   export function useEffect(effect: () => void | (() => void), deps?: ReadonlyArray<any>): void;
+  export function useRef<T>(initialValue: T): { current: T };
+  export function useRef<T = undefined>(): { current: T | undefined };
 }
