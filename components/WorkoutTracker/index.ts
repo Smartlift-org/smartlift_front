301d2e519c7e8e82d66db9f66428b9d5a2337d61
Fede <fcmde1995@gmail.com>
2025-06-29 01:57:40 -0300

    Remove obsolete SetsList component and update exports

--- components/WorkoutTracker/index.ts
+++ components/WorkoutTracker/index.ts
@@ -4,4 +4,4 @@ export { default as WorkoutControls } from './WorkoutControls';
 export { default as PauseReasonModal } from './PauseReasonModal';
 export { default as CompleteWorkoutModal } from './CompleteWorkoutModal';
 export { default as ConfirmModal } from './ConfirmModal';
-export { default as SetsList } from './SetsList';
+
